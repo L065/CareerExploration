c7e24e4cab67b6b5c8b81c171633a009e0424d42
L065 <luffy121020@gmail.com>
2024-01-01 22:49:26 +0800

    first build and Git 20240101

---
+++ src/main/java/com/fivestar/CareerExploration/CareerExplorationApplication.java
@@ -0,0 +1,13 @@
+package com.fivestar.CareerExploration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerExplorationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerExplorationApplication.class, args);
+	}
+
+}
